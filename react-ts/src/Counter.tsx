ef928ba5722c50cf4d496bbd23e92a76c85a1c8b
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2021-11-15 10:57:01 +0900

    리액트 컴포넌트 타입스크립트로 작성하기

--- react-ts/src/Counter.tsx
+++ react-ts/src/Counter.tsx
@@ -1,27 +1,27 @@
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 function Counter() {
-  // generics를 통해 타입 설정, 하지만 useState가 알아서 유추하기 때문에 해도 되고 안해도 된다.
-  const [count, setCount] = useState<number>(0);
-  const onIncrease = () => setCount(count + 1);
-  const onDecrease = () => setCount(count - 1);
-  
-  // 하지만, 상태가 null일 수도 있고 아닐 수도 있는 것엔 타입을 명시해야한다.
-  type Information = { name: string; description: string };
-  const [info, setInfo] = useState<Information | null>(null);
+    // generics를 통해 타입 설정, 하지만 useState가 알아서 유추하기 때문에 해도 되고 안해도 된다.
+    const [count, setCount] = useState<number>(0);
+    const onIncrease = () => setCount(count + 1);
+    const onDecrease = () => setCount(count - 1);
 
-  // 상태의 타입이 까다로운 경우에도 명시하는것이 좋다.
-  type Todo = { id: number; text: string; done: boolean };
-  const [todo, setTodo] = useState<Todo[]>([]);
+    // 하지만, 상태가 null일 수도 있고 아닐 수도 있는 것엔 타입을 명시해야한다.
+    type Information = { name: string; description: string };
+    const [info, setInfo] = useState<Information | null>(null);
 
-  return (
-    <div className="counter">
-      <h2>Counter</h2>
-      <p>{count}</p>
-      <button onClick={onIncrease}>+</button>
-      <button onClick={onDecrease}>-</button>
-    </div>
-  );
+    // 상태의 타입이 까다로운 경우에도 명시하는것이 좋다.
+    type Todo = { id: number; text: string; done: boolean };
+    const [todo, setTodo] = useState<Todo[]>([]);
+
+    return (
+        <div className="counter">
+            <h2>Counter</h2>
+            <p>{count}</p>
+            <button onClick={onIncrease}>+</button>
+            <button onClick={onDecrease}>-</button>
+        </div>
+    );
 }
 
 export default Counter;
--- react-ts/src/Greetings.tsx
+++ react-ts/src/Greetings.tsx
@@ -1,25 +1,26 @@
 import React from "react";
 
 type GreetingsProps = {
-  name: string;
-  mark: string;
-  optional?: string;
-  onClick: (name: string) => void;
+    name: string;
+    mark: string;
+    optional?: string;
+    onClick: (name: string) => void;
 };
+// 컴포넌트의 props 중에서 생략해도 되는 값이 있다면 ? 문자를 사용하면 된다.
 
 function Greetings({ name, mark, optional, onClick }: GreetingsProps) {
-  const handleClick = () => onClick(name);
-  return (
-    <div>
-      <h2>Greetings</h2>
-      Hello, {name}, {mark}
-      {optional && <p onClick={handleClick} >{optional}</p>}
-    </div>
-  );
+    const handleClick = () => onClick(name);
+    return (
+        <div>
+            <h2>Greetings</h2>
+            Hello, {name}, {mark}
+            {optional && <p onClick={handleClick}>{optional}</p>}
+        </div>
+    );
 }
 
 Greetings.defaultProps = {
-  mark: '!'
+    mark: "!",
 };
 
 export default Greetings;
